c909ad5ef02098dbd5baf1f9205d38ea12feadb5
Pritam688Pal <pritampal688@gmail.com>
2026-06-21 01:39:07 +0530

    Enhance add, commit, and init functionalities; add UUID dependency

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -2,16 +2,16 @@ const fs = require("fs").promises;
 const path = require("path");
 
 async function addFiles(filePath) {
-  const repoPath = path.resolve(process.cwd(), ".ZYXgit");
-  const stagingPath = path.join(repoPath, "staging");
-  try {
-    await fs.mkdir(stagingPath, { recursive: true });
-    const fileName = path.basename(filePath);
-    await fs.copyFile(filePath, path.join(stagingPath, fileName));
-    console.log(`File ${fileName} added to the staging area!`);
-  } catch (error) {
-    console.log("Error adding file : ", error);
-  }
+	const repoPath = path.resolve(process.cwd(), ".ZYXgit");
+	const stagingPath = path.join(repoPath, "staging");
+	try {
+		await fs.mkdir(stagingPath, { recursive: true });
+		const fileName = path.basename(filePath);
+		await fs.copyFile(filePath, path.join(stagingPath, fileName));
+		console.log(`File ${fileName} added to the staging area!`);
+	} catch (error) {
+		console.log("Error adding file : ", error);
+	}
 }
 
 module.exports = { addFiles };
--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -2,20 +2,20 @@ const fs = require("fs").promises;
 const path = require("path");
 
 async function initRepo(params) {
-  const repoPath = path.resolve(process.cwd(), ".ZYXgit");
-  const commitPath = path.join(repoPath, "commits");
+	const repoPath = path.resolve(process.cwd(), ".ZYXgit");
+	const commitPath = path.join(repoPath, "commits");
 
-  try {
-    await fs.mkdir(repoPath, { recursive: true });
-    await fs.mkdir(commitPath, { recursive: true });
-    await fs.writeFile(
-      path.join(repoPath, "config.json"),
-      JSON.stringify({ bucket: process.env.S3_BUCKET }),
-    );
-    console.log("Repository initialized!");
-  } catch (error) {
-    console.error("error initialising repository", err);
-  }
+	try {
+		await fs.mkdir(repoPath, { recursive: true });
+		await fs.mkdir(commitPath, { recursive: true });
+		await fs.writeFile(
+			path.join(repoPath, "config.json"),
+			JSON.stringify({ bucket: process.env.S3_BUCKET }),
+		);
+		console.log("Repository initialized!");
+	} catch (error) {
+		console.error("error initialising repository", err);
+	}
 }
 
 module.exports = { initRepo };
