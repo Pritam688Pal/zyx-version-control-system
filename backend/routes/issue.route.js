76e8bfac95335ff7fedbff64f337563bea4cb8d8
Pritam688Pal <pritampal688@gmail.com>
2026-06-23 22:50:33 +0530

    fix: Remove trailing slashes from update and delete routes in issue router

--- backend/routes/issue.route.js
+++ backend/routes/issue.route.js
@@ -2,8 +2,8 @@ const issueRouter = require("express").Router();
 const issueController = require("../controllers/issue.controller");
 
 issueRouter.post("/create", issueController.createIssue);
-issueRouter.put("/update/", issueController.updateIssueById);
-issueRouter.delete("/delete/", issueController.deleteIssueById);
+issueRouter.put("/update", issueController.updateIssueById);
+issueRouter.delete("/delete", issueController.deleteIssueById);
 issueRouter.get("/all", issueController.getAllIssues);
 issueRouter.get("/", issueController.getIssueById);
 
