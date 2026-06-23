2284abf306617954eaf8f1d2b570e35b73577f27
Pritam688Pal <pritampal688@gmail.com>
2026-06-23 12:00:40 +0530

    feat: Add Mongoose models for Issue, Repo, and User

---
+++ backend/models/issue.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const issueSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: Boolean,
+		default: true,
+	},
+	repository: {
+		type: Schema.Types.ObjectId,
+		ref: "Repository",
+		required: true,
+	},
+});
+
+const Issue = mongoose.model("Repo", issueSchema);
+
+export default Issue;
